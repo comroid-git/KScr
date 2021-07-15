9ca4a9747e0ec2b3c948e39f6e6e2f1689fe238c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-15 21:28:54 +0200

    Introduce Token class

---
+++ kscr-lib/Token.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "Modifier.h"
+
+enum TokenType : int
+{
+	Nop = 0x0,
+	Op = 0x1,
+	Mod = 0x2,
+	Ident = 0x4,
+	Num = 0x8,
+	Str = 0xF
+};
+
+struct Token
+{
+	explicit Token(TokenType type, Modifier modifier) : type(type), modifier(modifier) {}
+	explicit Token(TokenType type, Modifier modifier, std::string arg) : type(type), modifier(modifier), arg(std::move(arg)) {}
+	const TokenType type;
+	const Modifier modifier;
+	const std::string arg;
+	const std::vector<Token> sub = std::vector<Token>();
+};
