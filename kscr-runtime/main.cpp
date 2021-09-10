2d369c99f7eabcb09707e6f250450318b6bebb61
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-10 23:10:36 +0200

    Update main.cpp

--- kscr-runtime/main.cpp
+++ kscr-runtime/main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -19,17 +20,44 @@ int run()
 // eval test
 auto main(int argc, char* argv[]) -> int
 {
-	// debug code statement
-	const std::string code = "num a = 1; num b = 2.2; return a + b;";
+	try
+	{
+		// debug code statement
+		const std::string code = "num a = 1; num b = 2.2; return a + b;";
 
-	// parse code into tokens
-	const std::vector<Token> tokens = Eval::tokenize(code.data(), code.length());
+		// parse code into tokens
+		const std::vector<Token> tokens = Eval::tokenize(code.data(), code.length());
 
-	// compile tokens into bytecode
-	BytecodePacket bytecode = Eval::compile(&tokens);
+		// compile tokens into bytecode
+		BytecodePacket bytecode = Eval::compile(&tokens);
 
-	// run bytecode
-	return Eval::execute(&bytecode);
+		// run bytecode
+		return Eval::execute(&bytecode);
+	}
+	catch (std::exception& e)
+	{
+		std::cerr
+		<< "An internal exception occurred:" << std::endl
+		<< "\t- " << std::string(e.what()) << std::endl;
+		return -1;
+	}
+	catch (const int i) {
+		std::cerr << "An exit code was thrown: " << i << std::endl;
+		return i;
+	}
+	catch (const long l) {
+		std::cerr << "An exit code was thrown: " << l << std::endl;
+		return l;
+	}
+	catch (const char* p) {
+		std::cerr
+		<< "An internal message was thrown:" << std::endl
+		<< "\t- " << std::string(p) << std::endl;
+		return -1;
+	}
+	catch (...) {
+		std::cout << "nope, sorry, I really have no clue what that is\n";
+	}
 }
 
 /* // lib test
