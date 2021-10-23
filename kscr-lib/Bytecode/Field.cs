3f79f5a29f899b457ec88d212d95c0cceea7e9c2
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-23 17:19:06 +0200

    Update Field.cs

--- kscr-lib/Bytecode/Field.cs
+++ kscr-lib/Bytecode/Field.cs
@@ -1,9 +1,16 @@
-﻿namespace KScr.Lib.Bytecode
+﻿using KScr.Lib.Store;
+
+namespace KScr.Lib.Bytecode
 {
     public sealed class Field : AbstractClassMember
     {
         public Field(Class parent, string name, MemberModifier modifier) : base(parent, name, modifier)
         {
+        }
+
+        public override IRuntimeSite Evaluate(RuntimeBase vm, ref State state, ref ObjectRef rev)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
