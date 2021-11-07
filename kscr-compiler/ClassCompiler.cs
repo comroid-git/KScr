23dd0627241f73e9e5d12050fbcb4d1fe1fb16cc
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-11-07 19:06:36 +0100

    move compilers

---
+++ kscr-compiler/ClassCompiler.cs
@@ -0,0 +1,13 @@
+﻿using KScr.Lib;
+using KScr.Lib.Model;
+
+namespace KScr.Compiler
+{
+    public class ClassCompiler : AbstractCompiler
+    {
+        public override ICompiler? AcceptToken(RuntimeBase vm, ref CompilerContext ctx)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ kscr-compiler/CodeCompiler.cs
@@ -0,0 +1,23 @@
+﻿using KScr.Lib;
+using KScr.Lib.Model;
+
+namespace KScr.Compiler
+{
+    public abstract class AbstractCodeCompiler : AbstractCompiler {}
+    
+    public class StatementCompiler : AbstractCodeCompiler
+    {
+        public override ICompiler? AcceptToken(RuntimeBase vm, ref CompilerContext ctx)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
+    public class ExpressionCompiler : AbstractCodeCompiler
+    {
+        public override ICompiler? AcceptToken(RuntimeBase vm, ref CompilerContext ctx)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
