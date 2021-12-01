bf941a5b8c9afb56f67bcc94b2174d416f1c078e
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-12-01 13:51:05 +0100

    basic implementation for ParameterDefinitionCompiler

--- kscr-compiler/Class/ParameterDefinitionCompiler.cs
+++ kscr-compiler/Class/ParameterDefinitionCompiler.cs
@@ -1,5 +1,7 @@
-﻿using KScr.Lib;
+﻿using System.Collections.Generic;
+using KScr.Lib;
 using KScr.Lib.Bytecode;
+using KScr.Lib.Exception;
 using KScr.Lib.Model;
 
 namespace KScr.Compiler.Class
@@ -7,6 +9,7 @@ namespace KScr.Compiler.Class
     public class ParameterDefinitionCompiler : AbstractCompiler
     {
         private readonly Method _method;
+        private int pIndex = -1, pState = 0;
         private bool _active = true;
 
         public ParameterDefinitionCompiler(ClassCompiler parent, Method method) : base(parent)
@@ -21,6 +24,22 @@ namespace KScr.Compiler.Class
             switch (ctx.Token.Type)
             {
                 case TokenType.Word:
+                    if (pState == 0)
+                    { // parse type
+                        if (pIndex >= _method.Parameters.Count)
+                            throw new CompilerException("Invalid parameter index during compilation");
+                        
+                        _method.Parameters.Add(new MethodParameter());
+                        _method.Parameters[++pIndex].Type = vm.FindType(ctx.Token.Arg!)!;
+                        pState = 1;
+                    } else if (pState == 1)
+                    { // parse name
+                        _method.Parameters[pIndex].Name = ctx.Token.Arg!;
+                        pState = 0;
+                    }
+                    break;
+                case TokenType.Comma:
+                    pIndex += 1;
                     break;
                 case TokenType.ParRoundClose:
                     _active = false;
