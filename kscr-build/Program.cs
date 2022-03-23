9910b7ffab313e221a9a851958dfd9b42d259db2
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-03-23 08:38:46 +0100

    add build module

---
+++ kscr-build/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KScr.Build
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException("Build system is not implemented");
+        }
+    }
+}
