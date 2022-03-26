3c3cfb9d44199543c1753e8dcd0354860e06bd38
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-03-26 20:36:38 +0100

    implement other basic statement tests

--- kscr-test/StatementTest.cs
+++ kscr-test/StatementTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using KScr.Lib;
+using KScr.Lib.Core;
+using KScr.Lib.Exception;
 using NUnit.Framework;
 
 namespace KScr.Test
@@ -8,15 +10,27 @@ namespace KScr.Test
     public class StatementTest
     {
         public static readonly Random rng = new();
+        private TextWriter bakWrt;
+
+        [SetUp]
+        public void setup()
+        {
+            bakWrt = Console.Out;
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            Console.SetOut(bakWrt);
+        }
 
         [Test]
         public void TestReturn()
         {
             int desiredCode = rng.Next() % 16;
 
-            TestUtil.RunSourcecode($"return {desiredCode};");
-            Console.WriteLine($"test: ExitCode == {desiredCode}");
-            Assert.AreEqual(desiredCode, RuntimeBase.ExitCode);
+            var code = TestUtil.RunSourcecode($"return {desiredCode};");
+            Assert.AreEqual(desiredCode, (code.Value as Numeric).IntValue);
         }
 
         [Test]
@@ -26,47 +40,100 @@ namespace KScr.Test
 
             var writer = new StringWriter();
             Console.SetOut(writer);
-            TestUtil.RunSourcecode($"throw {desiredCode};");
-            var expected = "";
+            try
+            {
+                TestUtil.RunSourcecode($"throw {desiredCode};");
+            }
+            catch (InternalException expected)
+            {
+            }
+
+            var expectedOut = "";
 
             Console.WriteLine($"test: ExitCode == {desiredCode}");
             Assert.AreEqual(desiredCode, RuntimeBase.ExitCode);
+            Assert.IsTrue(writer.ToString().StartsWith(expectedOut), $"Expected output was:\n{expectedOut}\nActual Output was \n{writer}");
         }
 
         [Test]
         public void TestDeclaration()
         {
-            TestUtil.RunSourcecode("");
+            int desired = rng.Next() % 16;
+            
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var code = TestUtil.RunSourcecode($"public static int main() {{ int x = {desired}; return x; }}");
+            
+            Assert.AreEqual(desired, (code.Value as Numeric).IntValue);
         }
 
         [Test]
         public void TestIf()
         {
-            TestUtil.RunSourcecode("");
+            int desired = rng.Next() % 16;
+            
+            var code = TestUtil.RunSourcecode($"public static int main() {{ int x = {desired}; if (x > 8) return x; return x * 2; }}");
+            
+            Assert.AreEqual(desired > 8 ? desired : desired * 2, (code.Value as Numeric).IntValue);
         }
 
         [Test]
         public void TestIfElse()
         {
-            TestUtil.RunSourcecode("");
+            int desired = rng.Next() % 16;
+            
+            var code = TestUtil.RunSourcecode($"public static int main() {{ int x = {desired}; if (x % 2) {{ return x; }} else return x * 2; }}");
+            
+            Assert.AreEqual(desired % 2 > 0 ? desired : desired * 2, (code.Value as Numeric).IntValue);
         }
 
         [Test]
         public void TestFor()
         {
-            TestUtil.RunSourcecode("");
+            int desiredLen = rng.Next() % 64;
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var code = TestUtil.RunSourcecode($"for (int i = {desiredLen}; i; i -= 1) stdio << i;");
+            string expected = "";
+            for (int i = desiredLen; i > 0; i -= 1)
+                expected += $"{i}\n";
+            expected = expected.Substring(0, expected.Length - 1);
+
+            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
         }
 
         [Test]
         public void TestForEach()
         {
-            TestUtil.RunSourcecode("");
+            int desiredLen = rng.Next() % 64;
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var code = TestUtil.RunSourcecode($"foreach (i : 0~{desiredLen}) stdio << i;");
+            string expected = "";
+            for (int i = 0; i < desiredLen; i++)
+                expected += $"{i}\n";
+            expected = expected.Substring(0, expected.Length - 1);
+
+            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
         }
 
         [Test]
         public void TestWhile()
         {
-            TestUtil.RunSourcecode("");
+            int desiredLen = rng.Next() % 64;
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var code = TestUtil.RunSourcecode($"int i = {desiredLen}; while (i--) stdio << i;");
+            string expected = "";
+            int i = desiredLen;
+            while (i-- > 0)
+                expected += $"{i}\n";
+            expected = expected.Substring(0, expected.Length - 1);
+
+            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
         }
     }
 }
--- kscr-test/TestUtil.cs
+++ kscr-test/TestUtil.cs
@@ -1,17 +1,18 @@
-﻿using KScr.Runtime;
+﻿using KScr.Lib.Store;
+using KScr.Runtime;
 
 namespace KScr.Test
 {
     public class TestUtil
     {
-        public static void RunSourcecode(string code)
+        public static IObjectRef RunSourcecode(string code)
         {
             var vm = Program.VM;
             if (!code.Contains("main()"))
                 code = "public static void main() { " + code + " }";
             code = "package org.comroid.kscr.test; public class TestClass { " + code + " }";
             vm.CompileClass("TestClass", code);
-            vm.Execute();
+            return vm.Execute()[StackOutput.Omg] ?? vm.ConstantVoid;
         }
     }
 }
