9f02613dc7c5ece9fdc6123b08f2fa31bfafd13c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-03-26 10:20:18 +0100

    add native implementation class for File

---
+++ kscr-system/Impl/IO.cs
@@ -0,0 +1,21 @@
+﻿using KScr.Lib;
+using KScr.Lib.Core;
+using KScr.Lib.Store;
+
+namespace KScr.Native.System.Impl;
+
+[NativeImpl(Package = "org.comroid.kscr.io", ClassName = "File")]
+public class File
+{
+    [NativeImpl]
+    public static IObjectRef read(RuntimeBase vm, Stack stack, params IObject[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    [NativeImpl]
+    public static IObjectRef write(RuntimeBase vm, Stack stack, params IObject[] args)
+    {
+        throw new NotImplementedException();
+    }
+}
