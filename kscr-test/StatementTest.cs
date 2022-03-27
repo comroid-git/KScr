5386b1a74160805dd49241c4af4a30103e644aa9
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-03-27 12:56:43 +0200

    Update StatementTest.cs

--- kscr-test/StatementTest.cs
+++ kscr-test/StatementTest.cs
@@ -7,173 +7,172 @@ using KScr.Runtime;
 using NUnit.Framework;
 using static KScr.Test.TestUtil;
 
-namespace KScr.Test
+namespace KScr.Test;
+
+public class StatementTest
 {
-    public class StatementTest
+    public static readonly Random rng = new();
+    private TextWriter bakWrt;
+
+    [SetUp]
+    public void setup()
     {
-        public static readonly Random rng = new();
-        private TextWriter bakWrt;
+        Program.VM.Clear();
+        bakWrt = Console.Out;
+    }
 
-        [SetUp]
-        public void setup()
+    [TearDown]
+    public void teardown()
+    {
+        Console.SetOut(bakWrt);
+    }
+
+    [Test, Repeat(TestRepeat)]
+    public void TestReturn()
+    {
+        int desiredCode = rng.Next() % RngMax;
+        if (desiredCode < 10)
+            desiredCode = RngMax;
+
+        var code = RunSourcecode($"return {desiredCode};");
+        Assert.AreEqual(desiredCode, (code.Value as Numeric).IntValue);
+    }
+
+    [Test, Repeat(TestRepeat)]
+    public void TestThrow()
+    {
+        int desiredCode = rng.Next() % RngMax;
+        if (desiredCode < 10)
+            desiredCode = RngMax;
+
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        try
         {
-            Program.VM.Clear();
-            bakWrt = Console.Out;
+            RunSourcecode($"throw {desiredCode};");
         }
-
-        [TearDown]
-        public void teardown()
+        catch (InternalException expected)
         {
-            Console.SetOut(bakWrt);
+            var expectedOut = "";
+
+            Console.WriteLine($"test: ExitCode == {desiredCode}");
+            Assert.AreEqual(desiredCode, RuntimeBase.ExitCode);
+            Assert.IsTrue(writer.ToString().StartsWith(expectedOut), $"Expected output was:\n{expectedOut}\nActual Output was \n{writer}");
+            Assert.Pass();
+            return;
         }
+        Assert.Fail("Did not throw");
+    }
 
-        [Test, Repeat(TestRepeat)]
-        public void TestReturn()
-        {
-            int desiredCode = rng.Next() % RngMax;
-            if (desiredCode < 10)
-                desiredCode = RngMax;
-
-            var code = RunSourcecode($"return {desiredCode};");
-            Assert.AreEqual(desiredCode, (code.Value as Numeric).IntValue);
-        }
-
-        [Test, Repeat(TestRepeat)]
-        public void TestThrow()
-        {
-            int desiredCode = rng.Next() % RngMax;
-            if (desiredCode < 10)
-                desiredCode = RngMax;
-
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            try
-            {
-                RunSourcecode($"throw {desiredCode};");
-            }
-            catch (InternalException expected)
-            {
-                var expectedOut = "";
-
-                Console.WriteLine($"test: ExitCode == {desiredCode}");
-                Assert.AreEqual(desiredCode, RuntimeBase.ExitCode);
-                Assert.IsTrue(writer.ToString().StartsWith(expectedOut), $"Expected output was:\n{expectedOut}\nActual Output was \n{writer}");
-                Assert.Pass();
-                return;
-            }
-            Assert.Fail("Did not throw");
-        }
-
-        [Test, Repeat(TestRepeat)]
-        public void TestDeclaration()
-        {
-            int desired = rng.Next() % RngMax;
-            if (desired < 10)
-                desired = RngMax;
+    [Test, Repeat(TestRepeat)]
+    public void TestDeclaration()
+    {
+        int desired = rng.Next() % RngMax;
+        if (desired < 10)
+            desired = RngMax;
             
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            var code = RunSourcecode($"public static int main() {{ int x = {desired}; return x; }}");
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var code = RunSourcecode($"public static int main() {{ int x = {desired}; return x; }}");
             
-            Assert.AreEqual(desired, (code.Value as Numeric).IntValue);
-        }
+        Assert.AreEqual(desired, (code.Value as Numeric).IntValue);
+    }
 
-        [Test, Repeat(TestRepeat)]
-        public void TestIf()
-        {
-            int desired = rng.Next() % RngMax;
-            if (desired < 10)
-                desired = RngMax;
+    [Test, Repeat(TestRepeat)]
+    public void TestIf()
+    {
+        int desired = rng.Next() % RngMax;
+        if (desired < 10)
+            desired = RngMax;
             
-            var code = RunSourcecode($"public static int main() {{ int x = {desired}; if (x > 8) return x; return x * 2; }}");
+        var code = RunSourcecode($"public static int main() {{ int x = {desired}; if (x > 8) return x; return x * 2; }}");
             
-            Assert.AreEqual(desired > 8 ? desired : desired * 2, (code.Value as Numeric).IntValue);
-        }
+        Assert.AreEqual(desired > 8 ? desired : desired * 2, (code.Value as Numeric).IntValue);
+    }
 
-        [Test, Repeat(TestRepeat)]
-        public void TestIfElse()
-        {
-            int desired = rng.Next() % RngMax;
-            if (desired < 10)
-                desired = RngMax;
+    [Test, Repeat(TestRepeat)]
+    public void TestIfElse()
+    {
+        int desired = rng.Next() % RngMax;
+        if (desired < 10)
+            desired = RngMax;
             
-            var code = RunSourcecode($"public static int main() {{ int x = {desired}; if (x % 2) {{ return x; }} else return x * 2; throw x; }}");
+        var code = RunSourcecode($"public static int main() {{ int x = {desired}; if (x % 2) {{ return x; }} else return x * 2; throw x; }}");
             
-            Assert.AreEqual(desired % 2 > 0 ? desired : desired * 2, (code.Value as Numeric).IntValue);
-        }
+        Assert.AreEqual(desired % 2 > 0 ? desired : desired * 2, (code.Value as Numeric).IntValue);
+    }
 
-        [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
-        public void TestFor()
-        {
-            int desiredLen = rng.Next() % RngMax;
-            if (desiredLen < 10)
-                desiredLen = RngMax;
+    [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
+    public void TestFor()
+    {
+        int desiredLen = rng.Next() % RngMax;
+        if (desiredLen < 10)
+            desiredLen = RngMax;
 
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            var code = RunSourcecode($"for (int i = {desiredLen}; i; i -= 1) stdio << i;");
-            string expected = "";
-            for (int i = desiredLen; i > 0; i -= 1)
-                expected += $"{i}\n";
-            expected = expected.Substring(0, expected.Length - 1);
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var code = RunSourcecode($"for (int i = {desiredLen}; i; i -= 1) stdio << i;");
+        string expected = "";
+        for (int i = desiredLen; i > 0; i -= 1)
+            expected += $"{i}\n";
+        expected = expected.Substring(0, expected.Length - 1);
 
-            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
-        }
+        Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
+    }
 
-        [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
-        public void TestForEach()
-        {
-            int desiredLen = rng.Next() % RngMax;
-            if (desiredLen < 10)
-                desiredLen = RngMax;
+    [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
+    public void TestForEach()
+    {
+        int desiredLen = rng.Next() % RngMax;
+        if (desiredLen < 10)
+            desiredLen = RngMax;
 
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            var code = RunSourcecode($"foreach (i : 0~{desiredLen}) stdio << i;");
-            string expected = "";
-            for (int i = 0; i < desiredLen; i++)
-                expected += $"{i}\n";
-            expected = expected.Substring(0, expected.Length - 1);
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var code = RunSourcecode($"foreach (i : 0~{desiredLen}) stdio << i;");
+        string expected = "";
+        for (int i = 0; i < desiredLen; i++)
+            expected += $"{i}\n";
+        expected = expected.Substring(0, expected.Length - 1);
 
-            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
-        }
+        Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
+    }
 
-        [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
-        public void TestWhile()
-        {
-            int desiredLen = rng.Next() % RngMax;
-            if (desiredLen < 10)
-                desiredLen = RngMax;
+    [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
+    public void TestWhile()
+    {
+        int desiredLen = rng.Next() % RngMax;
+        if (desiredLen < 10)
+            desiredLen = RngMax;
 
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            var code = RunSourcecode($"int i = {desiredLen}; while (i--) stdio << i;");
-            string expected = "";
-            int i = desiredLen;
-            while (i-- > 0)
-                expected += $"{i}\n";
-            expected = expected.Substring(0, expected.Length - 1);
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var code = RunSourcecode($"int i = {desiredLen}; while (i--) stdio << i;");
+        string expected = "";
+        int i = desiredLen;
+        while (i-- > 0)
+            expected += $"{i}\n";
+        expected = expected.Substring(0, expected.Length - 1);
 
-            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
-        }
+        Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
+    }
 
-        [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
-        public void TestDoWhile()
-        {
-            int desiredLen = rng.Next() % RngMax;
-            if (desiredLen < 10)
-                desiredLen = RngMax;
+    [Test, Timeout(TestTimeout), Repeat(TestRepeat)]
+    public void TestDoWhile()
+    {
+        int desiredLen = rng.Next() % RngMax;
+        if (desiredLen < 10)
+            desiredLen = RngMax;
 
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            var code = RunSourcecode($"int i = {desiredLen}; do {{ stdio << i; }} while (i--);");
-            string expected = "";
-            int i = desiredLen;
-            do expected += $"{i}\n";
-            while (i-- > 0);
-            expected = expected.Substring(0, expected.Length - 1);
+        var writer = new StringWriter();
+        Console.SetOut(writer);
+        var code = RunSourcecode($"int i = {desiredLen}; do {{ stdio << i; }} while (i--);");
+        string expected = "";
+        int i = desiredLen;
+        do expected += $"{i}\n";
+        while (i-- > 0);
+        expected = expected.Substring(0, expected.Length - 1);
 
-            Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
-        }
+        Assert.IsTrue(writer.ToString().Replace("\r\n", "\n").StartsWith(expected), $"Expected output was:\n{expected}\nActual Output was \n{writer}");
     }
 }
