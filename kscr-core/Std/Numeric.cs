aee3771eec91bcd4c8ed5980db67fe83d63debf6
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-04-24 16:56:18 +0200

    Update Numeric.cs

--- kscr-core/Std/Numeric.cs
+++ kscr-core/Std/Numeric.cs
@@ -635,31 +635,6 @@ public sealed class Numeric : IObject
         return rev;
     }
 
-    public override string ToString()
-    {
-        return ToString(0);
-    }
-
-    public static string CreateKey(string num)
-    {
-        return "num:" + num;
-    }
-
-    public static string CreateKey(long num)
-    {
-        return CreateKey(num.ToString());
-    }
-
-    public static string CreateKey(float num)
-    {
-        return CreateKey(num.ToString(CultureInfo.InvariantCulture));
-    }
-
-    public static string CreateKey(double num)
-    {
-        return CreateKey(num.ToString(CultureInfo.InvariantCulture));
-    }
-
     public IObjectRef Sqrt(RuntimeBase vm)
     {
         switch (Mode)
@@ -730,6 +705,31 @@ public sealed class Numeric : IObject
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+    public override string ToString()
+    {
+        return ToString(0);
+    }
+
+    public static string CreateKey(string num)
+    {
+        return "num:" + num;
+    }
+
+    public static string CreateKey(long num)
+    {
+        return CreateKey(num.ToString());
+    }
+
+    public static string CreateKey(float num)
+    {
+        return CreateKey(num.ToString(CultureInfo.InvariantCulture));
+    }
+
+    public static string CreateKey(double num)
+    {
+        return CreateKey(num.ToString(CultureInfo.InvariantCulture));
     }
 }
 
