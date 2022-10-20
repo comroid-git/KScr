149c78f26a2e1fb6c25a408f9edd5765341fcbf5
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-20 09:29:47 +0200

    exmaple server

---
+++ kscr-lang-server/DidChangeWatchedFilesHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
+
+namespace KScr.LangServer;
+
+internal class DidChangeWatchedFilesHandler : IDidChangeWatchedFilesHandler
+{
+    public DidChangeWatchedFilesRegistrationOptions GetRegistrationOptions() => new DidChangeWatchedFilesRegistrationOptions();
+
+    public Task<Unit> Handle(DidChangeWatchedFilesParams request, CancellationToken cancellationToken) => Unit.Task;
+
+    public DidChangeWatchedFilesRegistrationOptions GetRegistrationOptions(DidChangeWatchedFilesCapability capability, ClientCapabilities clientCapabilities) => new DidChangeWatchedFilesRegistrationOptions();
+}
---
+++ kscr-lang-server/FoldingRangeHandler.cs
@@ -0,0 +1,33 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace KScr.LangServer;
+
+internal class FoldingRangeHandler : IFoldingRangeHandler
+{
+    public FoldingRangeRegistrationOptions GetRegistrationOptions() =>
+        new FoldingRangeRegistrationOptions {
+            DocumentSelector = DocumentSelector.ForLanguage("csharp")
+        };
+
+    public Task<Container<FoldingRange>?> Handle(
+        FoldingRangeRequestParam request,
+        CancellationToken cancellationToken
+    ) =>
+        Task.FromResult<Container<FoldingRange>?>(
+            new Container<FoldingRange>(
+                new FoldingRange {
+                    StartLine = 10,
+                    EndLine = 20,
+                    Kind = FoldingRangeKind.Region,
+                    EndCharacter = 0,
+                    StartCharacter = 0
+                }
+            )
+        );
+
+    public FoldingRangeRegistrationOptions GetRegistrationOptions(FoldingRangeCapability capability, ClientCapabilities clientCapabilities) => new FoldingRangeRegistrationOptions {
+        DocumentSelector = DocumentSelector.ForLanguage("csharp")
+    };
+}
