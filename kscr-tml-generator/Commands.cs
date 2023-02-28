f831f0bc27086bc811ad832b48f34bf42fe96c02
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-02-28 15:56:14 +0100

    add tml generator

---
+++ kscr-tml-generator/Commands.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace KScr.TmlGenerator;
+
+public interface IBaseCmd
+{
+    [Option('o', "output", Default = "kscr.tmLanguage")]
+    public FileInfo OutputFile { get; set; }
+}
+
+[Verb("gen", isDefault: true)]
+public class GenerateCommand : IBaseCmd
+{
+    public FileInfo OutputFile { get; set; }
+}
---
+++ kscr-tml-generator/KScrTmlGenerator.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using KScr.Antlr;
+
+namespace KScr.TmlGenerator;
+
+public static class KScrTmlGenerator
+{
+    public static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<GenerateCommand>(args)
+            .WithParsed(RunGenerate);
+    }
+
+    private static void RunGenerate(GenerateCommand obj)
+    {
+    }
+}
