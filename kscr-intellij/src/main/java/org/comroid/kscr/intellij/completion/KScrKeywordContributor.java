0a3fa8efa55a5782dd2cd4bb35660b7985ef08f7
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-03-02 19:33:57 +0100

    introduce keyword contributor

--- kscr-intellij/src/main/java/org/comroid/kscr/intellij/completion/KScrKeywordContributor.java
+++ kscr-intellij/src/main/java/org/comroid/kscr/intellij/completion/KScrKeywordContributor.java
@@ -1,16 +1,46 @@
 package org.comroid.kscr.intellij.completion;
 
-import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.patterns.PlatformPatterns;
+import org.comroid.kscr.intellij.antlr_generated.KScrLexer;
+import org.comroid.kscr.intellij.psi.Tokens;
 import org.jetbrains.annotations.NotNull;
 
-public class KScrKeywordContributor extends CompletionContributor {
-    public KScrKeywordContributor() {
-    }
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+public class KScrKeywordContributor extends CompletionContributor {
     @Override
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+        Stream<? extends Element> contextResults = Stream.empty();
+        Stream.concat(
+                IntStream.concat(IntStream.concat(
+                                Arrays.stream(Tokens.LiteralsTokens),
+                                Arrays.stream(Tokens.PrimitiveTypeTokens)
+                        ), Arrays.stream(Tokens.InfrastructureTokens))
+                        .mapToObj(KScrLexer.VOCABULARY::getLiteralName)
+                        .filter(Objects::nonNull)
+                        .map(str -> str.substring(1, str.length() - 1))
+                        .map(Element::new),
+                        contextResults)
+                .forEachOrdered(result::addElement);
         super.fillCompletionVariants(parameters, result);
+    }
+
+    private static class Element extends LookupElement {
+        private final String str;
+
+        private Element(String str) {
+            this.str = str;
+        }
+
+        @Override
+        public @NotNull String getLookupString() {
+            return str;
+        }
     }
 }
--- kscr-intellij/src/main/java/org/comroid/kscr/intellij/psi/Tokens.java
+++ kscr-intellij/src/main/java/org/comroid/kscr/intellij/psi/Tokens.java
@@ -18,23 +18,22 @@ public final class Tokens {
     public static final List<TokenIElementType> TOKEN_ELEMENT_TYPES = PSIElementTypeFactory.getTokenIElementTypes(KScrLanguage.LANGUAGE);
     public static final List<RuleIElementType> RULE_ELEMENT_TYPES = PSIElementTypeFactory.getRuleIElementTypes(KScrLanguage.LANGUAGE);
 
-    public static final TokenSet Comment = $(SING_COMMENT);
-    public static final TokenSet PrimitiveType = $(
-            TYPE, VOID, OBJECT, ARRAYIDENT, TUPLEIDENT, ENUM, ANNOTATION,
-            BOOL, BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, NUMIDENT);
-    public static final TokenSet NumericLit = $(NUMLIT);
-    public static final TokenSet StringLit = $(STRLIT);
-    public static final TokenSet Literals = $(STDIOLIT, ENDLLIT, NULL, TRUE, FALSE);
-    public static final TokenSet Braces = $(LPAREN, LSQUAR, LBRACE, LESSER, GREATER, RBRACE, RSQUAR, RPAREN);
-    public static final TokenSet Infrastructure = $(SEMICOLON, COMMA,
-            PUBLIC, PROTECTED, INTERNAL, PRIVATE,
-            STATIC, ABSTRACT, FINAL, SYNCHRONIZED, NATIVE, SERVE,
-            WHERE, SELECT, ELSE,
-            IF, FOR, FOREACH, WHILE, DO, TRY, CATCH, NEW,
-            RETURN, THROW,
-            PACKAGE, IMPORT,
-            CLASS, ENUM, INTERFACE, ANNOTATION);
-    public static final TokenSet InfrastructureLow = $(COLON, DOT, LDASHARROW, LLDASHARROW, RRDASHARROW, RDASHARROW, RREQARROW, REQARROW);
+    public static final int[] CommentTokens = new int[]{SING_COMMENT};
+    public static final int[] PrimitiveTypeTokens = new int[]{TYPE, VOID, OBJECT, ARRAYIDENT, TUPLEIDENT, ENUM, ANNOTATION, BOOL, BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, NUMIDENT};
+    public static final int[] NumericLitTokens = new int[]{NUMLIT};
+    public static final int[] StringLitTokens = new int[]{STRLIT};
+    public static final int[] LiteralsTokens = new int[]{STDIOLIT, ENDLLIT, NULL, TRUE, FALSE};
+    public static final int[] BracesTokens = new int[]{LPAREN, LSQUAR, LBRACE, LESSER, GREATER, RBRACE, RSQUAR, RPAREN};
+    public static final int[] InfrastructureTokens = new int[]{SEMICOLON, COMMA, PUBLIC, PROTECTED, INTERNAL, PRIVATE, STATIC, ABSTRACT, FINAL, SYNCHRONIZED, NATIVE, SERVE, WHERE, SELECT, ELSE, IF, FOR, FOREACH, WHILE, DO, TRY, CATCH, NEW, RETURN, THROW, PACKAGE, IMPORT, CLASS, ENUM, INTERFACE, ANNOTATION};
+    public static final int[] InfrastructureLowTokens = new int[]{COLON, DOT, LDASHARROW, LLDASHARROW, RRDASHARROW, RDASHARROW, RREQARROW, REQARROW};
+    public static final TokenSet Comment = $(CommentTokens);
+    public static final TokenSet PrimitiveType = $(PrimitiveTypeTokens);
+    public static final TokenSet NumericLit = $(NumericLitTokens);
+    public static final TokenSet StringLit = $(StringLitTokens);
+    public static final TokenSet Literals = $(LiteralsTokens);
+    public static final TokenSet Braces = $(BracesTokens);
+    public static final TokenSet Infrastructure = $(InfrastructureTokens);
+    public static final TokenSet InfrastructureLow = $(InfrastructureLowTokens);
 
     private static TokenSet $(int... types) {
         return PSIElementTypeFactory.createTokenSet(KScrLanguage.LANGUAGE, types);
